56f36c00965c53c8086a1a371d7981fd8f9f08dc
Anshika Rawat <101346883+Anshika4444@users.noreply.github.com>
2023-01-15 23:26:11 +0530

    Add files via upload

---
+++ floydwarshal.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <stdlib.h>
+using namespace std;
+void floyds(int b[][10],int n)
+{
+    int i, j, k;
+    for (k = 0; k < n; k++)
+    {
+        for (i = 0; i < n; i++)
+        {
+            for (j = 0; j < n; j++)
+            {
+                if ((b[i][k] * b[k][j] != 0) && (i != j))
+                {
+                    if ((b[i][k] + b[k][j] < b[i][j]) || (b[i][j] == 0))
+                    {
+                        b[i][j] = b[i][k] + b[k][j];
+                    }
+                }
+            }
+        }
+    }
+    for (i = 0; i < n; i++)
+    {
+        cout<<"\nMinimum Cost With Respect to Node:"<<i<<endl;
+        for (j = 0; j < n; j++)
+        {
+            cout<<b[i][j]<<"\t";
+        }
+
+    }
+}
+int main()
+{
+    int n;
+    cin>>n;
+    int b[10][10];
+    cout<<"ENTER VALUES OF ADJACENCY MATRIX\n\n";
+    for (int i = 0; i <n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            cin>>b[i][j];
+        }
+    }
+    floyds(b,n);
+}
